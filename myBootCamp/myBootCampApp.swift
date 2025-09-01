b68645bcff79ccbc60cdce2cf9574ccd21344227
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-08-31 21:35:30 -0700

    Initial Commit

---
+++ myBootCamp/myBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  myBootCampApp.swift
+//  myBootCamp
+//
+//  Created by Максим Минаков on 31.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct myBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myBootCampTests/myBootCampTests.swift
@@ -0,0 +1,17 @@
+//
+//  myBootCampTests.swift
+//  myBootCampTests
+//
+//  Created by Максим Минаков on 31.08.2025.
+//
+
+import Testing
+@testable import myBootCamp
+
+struct myBootCampTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
