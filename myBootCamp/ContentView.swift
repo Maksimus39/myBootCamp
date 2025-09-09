66f80155065bbb96eda7d67e8b179b82991505bc
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-09-09 12:36:36 -0700

    homework geometric shares

--- myBootCamp/ContentView.swift
+++ myBootCamp/ContentView.swift
@@ -3,9 +3,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        Circle()
-            .stroke(.green, lineWidth: 20)
-            .padding(20)
+        // task 1
+                Rectangle()
+                    .cornerRadius(20.0)
+                    .padding()
+                    .foregroundColor(.blue.opacity(0.5))
+        
+        
+        
+        //task 2
+        RoundedRectangle(cornerRadius: 20.0)
+            .stroke(lineWidth: 30)
+            .fill(LinearGradient(colors: [.blue, .purple],startPoint: .bottom, endPoint: .top))
+            .frame(width: 300, height: 300)
+        
+        
+        
     }
 }
 
