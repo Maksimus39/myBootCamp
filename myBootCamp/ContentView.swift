500ab39cd1b6083bd6ad1dea600ccdd274582f0b
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-09-09 12:14:52 -0700

    learn geometric shapes

--- myBootCamp/ContentView.swift
+++ myBootCamp/ContentView.swift
@@ -3,15 +3,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        Text("Мой первый текст в SwiftUI, и я рад что дошёл до этого раздела!")
-            .font(.largeTitle)
-            .bold()
-            .multilineTextAlignment(.leading)
-            .kerning(10)
-            .foregroundColor(Color("Color1"))
-            .frame(width: 300, height: 200, alignment: .center)
-            .minimumScaleFactor(0.4)
-            .textSelection(.enabled)
+        Circle()
+            .stroke(.green, lineWidth: 20)
+            .padding(20)
     }
 }
 
