341e216478005d48a3f6266b614d04b769362cdb
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-09-08 20:36:54 -0700

    lesson_1

--- myBootCamp/ContentView.swift
+++ myBootCamp/ContentView.swift
@@ -1,21 +1,17 @@
-//
-//  ContentView.swift
-//  myBootCamp
-//
-//  Created by Максим Минаков on 31.08.2025.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        Text("Мой первый текст в SwiftUI, и я рад что дошёл до этого раздела!")
+            .font(.largeTitle)
+            .bold()
+            .multilineTextAlignment(.leading)
+            .kerning(10)
+            .foregroundColor(Color("Color1"))
+            .frame(width: 300, height: 200, alignment: .center)
+            .minimumScaleFactor(0.4)
+            .textSelection(.enabled)
     }
 }
 
