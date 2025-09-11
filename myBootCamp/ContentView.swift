7a5c0bdefc9ab5b4c86bd3ab632cd06234381b35
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-09-11 14:40:12 -0700

    homework

--- myBootCamp/ContentView.swift
+++ myBootCamp/ContentView.swift
@@ -2,16 +2,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        Text("Мой первый текст в SwiftUI, и я рад что дошёл до этого раздела!")
-            .font(.largeTitle)
-            .bold()
-            .multilineTextAlignment(.leading)
-            .kerning(10)
-            .foregroundColor(Color("Color1"))
-            .frame(width: 300, height: 200, alignment: .center)
-            .minimumScaleFactor(0.4)
-            .textSelection(.enabled)
+      // task 1
+        Image(systemName:"bicycle.circle.fill")
+            .font(.system(size: 100))
+            .foregroundColor(.blue)
+        // task 2
+        Text("Добро пожаловать в приложение")
+            .foregroundColor(.white)
+            .frame(width: 300, height: 50)
+            .background(.black)
+            .cornerRadius(10)
+        // task 3
+        Rectangle()
+            .frame(width: 100, height: 100)
+            .frame(width: 200, height: 200)
+            .background(.yellow)
+            .frame(width: 300, height: 300)
+            .background(.blue)
+        // task 4
+        Rectangle()
+            .frame(width: 100, height: 100)
+            .frame(width: 200, height: 200, alignment: .topLeading)
+            .background(.yellow)
+            .frame(width: 300, height: 300, alignment: .topLeading)
+            .background(.blue)
+        // task 5
+       Text("I")
+            .font(.system(size: 150))
+            .italic()
+            .foregroundColor(.red)
+            .background(
+                Image(systemName: "bicycle.circle.fill")
+                    .font(.system(size: 100))
+                    .foregroundColor(.blue)
+                    .background(Circle()
+                        .foregroundColor(.white)
+                        .frame(width: 100, height: 100)
+                        .background(
+                            Circle()
+                                .stroke(lineWidth: 20)
+                                .foregroundColor(.red)
+                        )
+                    )
+            )
     }
 }
 
