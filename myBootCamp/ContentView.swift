db8bef513b964f818b5ddc367ba86e360c27e807
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-09-13 15:15:26 -0700

    homework HStack VStack ZStack

--- myBootCamp/ContentView.swift
+++ myBootCamp/ContentView.swift
@@ -3,52 +3,49 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        ZStack{
-            Color.red
-                .ignoresSafeArea()
-            
+        // --------------------- task 1
+        
+//        ZStack {
+//            Color.yellow
+//                .ignoresSafeArea()
+//            Text("Добро пожаловать")
+//        }
+        
+        // --------------------- task 2
+        
+//        VStack{
+//            Text("Новая игра")
+//            Text("Загрузить игру")
+//            Text("Сохранить игру")
+//            Text("Настройки")
+//            Text("Выход")
+//        }
+        
+        // ---------------------- task 3
+        
+//        VStack{
+//            Text("Вам уже есть 18?")
+//            HStack{
+//                Text("Да")
+//                Text("Нет")
+//            }
+//        }
+        
+        
+        // ---------------------- task 4
+        
+        VStack{
+            Text("Вам уже есть 18?")
             HStack{
-                VStack{
-                    Image(systemName: "camera.macro")
-                        .foregroundColor(.white)
-                        .font(.largeTitle)
-                        .frame(width: 100, height: 100)
-                        .background(.gray)
-                        .cornerRadius(20)
-                    
-                    HStack{
-                        Image(systemName: "camera.macro")
-                        Text("Каталог")
-                    }
-                    
-                }
-                VStack{
-                    Image(systemName: "cart.fill")
-                        .foregroundColor(.white)
-                        .font(.largeTitle)
-                        .frame(width: 100, height: 100)
-                        .background(.gray)
-                        .cornerRadius(20)
-                    
-                    HStack{
-                        Image(systemName: "cart.fill")
-                        Text("Корзина")
-                    }
-                    
-                }
-                VStack{
-                    Image(systemName: "person.fill")
-                        .foregroundColor(.white)
-                        .font(.largeTitle)
-                        .frame(width: 100, height: 100)
-                        .background(.gray)
-                        .cornerRadius(20)
-                    
-                    HStack{
-                        Image(systemName: "person.fill")
-                        Text("Профиль")
-                    }
-                }
+                Text("Да")
+                    .frame(width: 90, height: 30)
+                    .background(.green)
+                    .cornerRadius(18)
+                Text("Нет")
+                    .frame(width: 90, height: 30)
+                    .background(.red)
+                    .cornerRadius(18)
+                
             }
         }
     }
