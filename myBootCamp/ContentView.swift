b9b77a317de617ab7a9020d5ed44c6cac1e11791
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-09-17 22:27:31 -0700

    learn Spacer

--- myBootCamp/ContentView.swift
+++ myBootCamp/ContentView.swift
@@ -3,15 +3,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        Text("Мой первый текст в SwiftUI, и я рад что дошёл до этого раздела!")
-            .font(.largeTitle)
-            .bold()
-            .multilineTextAlignment(.leading)
-            .kerning(10)
-            .foregroundColor(Color("Color1"))
-            .frame(width: 300, height: 200, alignment: .center)
-            .minimumScaleFactor(0.4)
-            .textSelection(.enabled)
+        Spacer()
+        
+        VStack(spacing:0){
+            Spacer()
+            RoundedRectangle(cornerRadius: 25)
+            Spacer()
+            RoundedRectangle(cornerRadius: 25)
+            Spacer()
+        }
+        
+        VStack(spacing:0){
+            Spacer()
+            RoundedRectangle(cornerRadius: 25)
+            Spacer()
+            RoundedRectangle(cornerRadius: 25)
+            Spacer()
+        }
+        
+        Spacer()
     }
 }
 
