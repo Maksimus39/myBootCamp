d51284cfd969303a0d940794b8fd8a9837a4c6db
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-09-21 21:59:08 -0700

    usage variables

--- myBootCamp/ContentView.swift
+++ myBootCamp/ContentView.swift
@@ -1,17 +1,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var myText = "Мой пробный текст"
+    var myName: String = "Максим"
+    var myColor:Color = .black
+    
     var body: some View {
-        
-        Text("Мой первый текст в SwiftUI, и я рад что дошёл до этого раздела!")
-            .font(.largeTitle)
-            .bold()
-            .multilineTextAlignment(.leading)
-            .kerning(10)
-            .foregroundColor(Color("Color1"))
-            .frame(width: 300, height: 200, alignment: .center)
-            .minimumScaleFactor(0.4)
-            .textSelection(.enabled)
+        ZStack{
+            Color.red
+                .ignoresSafeArea()
+            VStack{
+                Text(myText)
+                Text("Меня зовут: \(myName)")
+                    .foregroundColor(myColor)
+            }
+        }
     }
 }
 
