177194b0a52cf2721d223116ff0d59dfe865e769
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-09-21 22:35:41 -0700

    Button()&@State

--- myBootCamp/ContentView.swift
+++ myBootCamp/ContentView.swift
@@ -1,17 +1,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var myColor: Color = .blue
+    
     var body: some View {
-        
-        Text("Мой первый текст в SwiftUI, и я рад что дошёл до этого раздела!")
-            .font(.largeTitle)
-            .bold()
-            .multilineTextAlignment(.leading)
-            .kerning(10)
-            .foregroundColor(Color("Color1"))
-            .frame(width: 300, height: 200, alignment: .center)
-            .minimumScaleFactor(0.4)
-            .textSelection(.enabled)
+        VStack(spacing: 20) {
+            // Прямоугольник, который меняет цвет
+            RoundedRectangle(cornerRadius: 25)
+                .fill(myColor)
+                .frame(width: 300, height: 300)
+                .padding()
+                .animation(.easeInOut(duration: 0.4), value: myColor)
+            
+            // Кнопки для изменения цвета
+            Button("Сменить цвет на красный") {
+                self.myColor = .red
+            }
+            .padding()
+            .background(Color.red)
+            .foregroundColor(.black)
+            .cornerRadius(10)
+            
+            Button("Сменить цвет на зелёный") {
+                self.myColor = .green
+            }
+            .padding()
+            .background(Color.green)
+            .foregroundColor(.black)
+            .cornerRadius(10)
+            
+            Button("Сменить цвет на синий") {
+                self.myColor = .blue
+            }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.black)
+            .cornerRadius(10)
+        }
     }
 }
 
