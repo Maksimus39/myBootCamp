060898fc8ce11b0001107c396f6162f92ff3d998
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-10-03 10:25:10 -0700

    refactoring

--- myBootCamp/ContentView.swift
+++ myBootCamp/ContentView.swift
@@ -1,44 +1,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    let trashItems = [
+        (systemName: "trash.fill", backgroundColor: Color.red),
+        (systemName: "document.on.trash.fill", backgroundColor: Color.yellow),
+        (systemName: "trash.square", backgroundColor: Color.green),
+        (systemName: "trash.circle.fill", backgroundColor: Color.purple),
+        (systemName: "figure.walk.diamond", backgroundColor: Color.blue)
+    ]
+    
     var body: some View {
-        
-        ScrollView (.horizontal, showsIndicators: false) {
-            HStack {
-                Image(systemName: "trash.fill")
-                    .foregroundColor(.white)
-                    .font(.largeTitle)
-                    .frame(width: 300, height: 300)
-                    .background(.red)
-                    .cornerRadius(25)
-                
-                Image(systemName: "document.on.trash.fill")
-                    .foregroundColor(.white)
-                    .font(.largeTitle)
-                    .frame(width: 300, height: 300)
-                    .background(.yellow)
-                    .cornerRadius(25)
-                
-                Image(systemName: "trash.square")
-                    .foregroundColor(.white)
-                    .font(.largeTitle)
-                    .frame(width: 300, height: 300)
-                    .background(.green)
-                    .cornerRadius(25)
-                
-                Image(systemName: "trash.circle.fill")
-                    .foregroundColor(.white)
-                    .font(.largeTitle)
-                    .frame(width: 300, height: 300)
-                    .background(.purple)
-                    .cornerRadius(25)
-                
-                Image(systemName: "figure.walk.diamond")
-                    .foregroundColor(.white)
-                    .font(.largeTitle)
-                    .frame(width: 300, height: 300)
-                    .background(.blue)
-                    .cornerRadius(25)
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack {
+                ForEach(trashItems, id: \.systemName) { item in
+                    Image(systemName: item.systemName)
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .frame(width: 300, height: 300)
+                        .background(item.backgroundColor)
+                        .cornerRadius(25)
+                }
             }
         }
     }
