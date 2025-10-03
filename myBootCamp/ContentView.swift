7e18d6f07ad5a064f716c3c3f65ac9e209d642b6
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-10-03 09:28:37 -0700

    input text

--- myBootCamp/ContentView.swift
+++ myBootCamp/ContentView.swift
@@ -1,17 +1,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var text: String = ""
+    @State private var name = ""
+    
+    
     var body: some View {
+        VStack{
+            TextField("Введите ваше имя", text: $text)
+            Text(text)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+                .foregroundColor(.red)
+        }
         
-        Text("Мой первый текст в SwiftUI, и я рад что дошёл до этого раздела!")
-            .font(.largeTitle)
-            .bold()
-            .multilineTextAlignment(.leading)
-            .kerning(10)
-            .foregroundColor(Color("Color1"))
-            .frame(width: 300, height: 200, alignment: .center)
-            .minimumScaleFactor(0.4)
-            .textSelection(.enabled)
+        Button(action: {
+            name = text
+        }, label: {
+            Text("Сохранить")
+                .font(.headline)
+                .padding()
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .background(.green)
+                .cornerRadius(10)
+                .padding()
+               
+        })
     }
 }
 
